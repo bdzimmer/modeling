dbe2a2fcadee6e38ceef1648f7c6981f15caed42
Ben Zimmer <ben.d.zimmer@gmail.com>
2021-07-08 21:51:34 -0500

    rearrange code

--- modeling/render_blender.py
+++ modeling/render_blender.py
@@ -5,13 +5,13 @@ Use Blender to render a scene to one or more image files.
 """
 
 # Copyright (c) 2021 Ben Zimmer. All rights reserved.
+
 import pickle
 
 import os
 import json
 import sys
-from typing import Any, Dict, Optional
-import math
+
 
 import bpy
 
@@ -21,29 +21,14 @@ CODE_DIRNAME = '/home/ben/code/modeling'
 if CODE_DIRNAME not in sys.path:
     sys.path.append(CODE_DIRNAME)
 
-from modeling import blender
+from modeling import blender, scene as msc
 
 DO_RENDER = True
-
-# TODO: figure out how to control the name of the default collection
-DEFAULT_COLLECTION = 'Collection'
 
 CYCLES_RENDER_SAMPLES = 8
 CYCLES_PREVIEW_SAMPLES = 8
 
 LIGHT_SUN = 'sun'
-
-
-def import_obj(filename) -> bpy.types.Object:
-    """import an obj file"""
-    bpy.ops.import_scene.obj(
-        filepath=filename,
-        use_smooth_groups=False,  # for now
-        split_mode='OFF',
-        axis_forward='Y',
-        axis_up='Z')
-    obj = bpy.context.selected_objects[0]
-    return obj
 
 
 def main(args):
@@ -99,10 +84,10 @@ def main(args):
     # ~~~~ load OBJ files
 
     # for now, just load starting from the first model
-    root_obj = add_model(config['models'][0], None)
+    root_obj = msc.add_model(config['models'][0], None)
     if len(config['models']) > 1:
         for model in config['models'][1:]:
-            add_model(model, None)
+            msc.add_model(model, None)
 
     # apply offset from center in configuration
     root_obj.location = root_obj_loc
@@ -122,7 +107,7 @@ def main(args):
     cam.name = "Camera"
     cam.data.clip_end = clip_end
 
-    set_transformation(cam, config['camera']['transformation'])
+    msc.set_transformation(cam, config['camera']['transformation'])
 
     bpy.context.scene.camera = cam
 
@@ -140,7 +125,7 @@ def main(args):
         else:
             print(f'Invalid light type')
 
-        set_transformation(light_obj, light['transformation'])
+        msc.set_transformation(light_obj, light['transformation'])
 
     # ~~~~ render settings
 
@@ -267,99 +252,6 @@ def render(output_filename: str) -> None:
     bpy.ops.render.render()
     bpy.data.images["Render Result"].save_render(filepath=output_filename)
     print('done')
-
-
-def add_model(
-        model_config: Dict[str, Any],
-        parent: Optional[bpy.types.Object]) -> bpy.types.Object:
-    """add a model to the blender scene"""
-
-    # Note that some logic in here for material assumes that model names are unique.
-
-    name = model_config['name']
-
-    model_filename = model_config.get('filename')
-    if model_filename is not None:
-        obj = import_obj(model_config['filename'])
-        obj.name = name
-    else:
-        obj = bpy.data.objects.new(name, None)
-        bpy.data.collections[DEFAULT_COLLECTION].objects.link(obj)
-
-    if parent is not None:
-        obj.parent = parent
-
-    transformation = model_config.get('transformation')
-    if transformation is not None:
-        set_transformation(obj, transformation)
-
-    # enable smooth shading
-    auto_smooth_angle = model_config.get('auto_smooth_angle')
-    if auto_smooth_angle is not None:
-        obj.data.use_auto_smooth = True
-        obj.data.auto_smooth_angle = auto_smooth_angle * math.pi / 180.0
-        bpy.ops.object.shade_smooth()
-
-    if obj.data is not None:
-
-        color = model_config.get('color')
-
-        # create a new material for the object
-        # not sure this is correct
-        # material = bpy.data.materials['Default OBJ']
-        material = bpy.data.materials.new(name=name)
-        material.use_nodes = True
-
-        if obj.data.materials:
-            obj.data.materials[0] = material
-        else:
-            obj.data.materials.append(material)
-
-        if color is not None:
-            material.node_tree.nodes['Principled BSDF'].inputs[0].default_value = color
-
-    children = model_config.get('children', [])
-    for child in children:
-        add_model(child, obj)
-
-    return obj
-
-
-def set_transformation(
-        obj: bpy.types.Object,
-        transf: Dict[str, str]) -> None:
-    """set the transformation of an object"""
-
-    trans = transf['translation']
-    rot = transf['rotation']
-
-    obj.location = trans
-    # blender.point_at(cam, root_obj, 'TRACK_NEGATIVE_Z', 'UP_Y')
-
-    if isinstance(rot, dict):
-        point_at_obj = blender.get_obj_by_name(rot['point_at'])
-        blender.point_at(
-            obj,
-            point_at_obj,
-            blender.TRACK_AXIS[rot.get('track_axis', '-z')],
-            blender.UP_AXIS[rot.get('up_axis', 'y')])
-    else:
-        if len(rot) == 3:
-            # I think Panda3D's HPR is intrinsic
-            # If Blender is extrinsic, the order can just be reversed, LOL
-            # rot is in HPR form
-            # H -> Z
-            # P -> X
-            # R -> Y
-            obj.rotation_mode = 'ZXY'
-            obj.rotation_euler = (
-                math.radians(rot[1]),  # X == (ZXY)[1]
-                math.radians(rot[2]),  # Y == (ZXY)[2]
-                math.radians(rot[0]),  # Z == (ZXY)[0]
-            )
-        else:
-            obj.rotation_mode = 'QUATERNION'
-            obj.rotation_quaternion = rot
 
 
 main(blender.find_args(sys.argv))
--- modeling/scene.py
+++ modeling/scene.py
@@ -16,3 +16,120 @@ Utilties for working with my own scene JSON format.
 #     to identity transformation if not present.
 # * children - a list of additional model objects that should be positioned
 #     relative to this one
+
+
+from typing import Any, Dict, Optional
+import math
+
+import bpy
+
+from modeling import blender
+
+
+# TODO: figure out how to control the name of the default collection
+DEFAULT_COLLECTION = 'Collection'
+
+
+def add_model(
+        model_config: Dict[str, Any],
+        parent: Optional[bpy.types.Object]) -> bpy.types.Object:
+    """add a model to the blender scene"""
+
+    # Note that some logic in here for material assumes that model names are unique.
+
+    name = model_config['name']
+
+    model_filename = model_config.get('filename')
+    if model_filename is not None:
+        obj = import_obj(model_config['filename'])
+        obj.name = name
+    else:
+        obj = bpy.data.objects.new(name, None)
+        bpy.data.collections[DEFAULT_COLLECTION].objects.link(obj)
+
+    if parent is not None:
+        obj.parent = parent
+
+    transformation = model_config.get('transformation')
+    if transformation is not None:
+        set_transformation(obj, transformation)
+
+    # enable smooth shading
+    auto_smooth_angle = model_config.get('auto_smooth_angle')
+    if auto_smooth_angle is not None:
+        obj.data.use_auto_smooth = True
+        obj.data.auto_smooth_angle = auto_smooth_angle * math.pi / 180.0
+        bpy.ops.object.shade_smooth()
+
+    if obj.data is not None:
+
+        color = model_config.get('color')
+
+        # create a new material for the object
+        # not sure this is correct
+        # material = bpy.data.materials['Default OBJ']
+        material = bpy.data.materials.new(name=name)
+        material.use_nodes = True
+
+        if obj.data.materials:
+            obj.data.materials[0] = material
+        else:
+            obj.data.materials.append(material)
+
+        if color is not None:
+            material.node_tree.nodes['Principled BSDF'].inputs[0].default_value = color
+
+    children = model_config.get('children', [])
+    for child in children:
+        add_model(child, obj)
+
+    return obj
+
+
+def set_transformation(
+        obj: bpy.types.Object,
+        transf: Dict[str, str]) -> None:
+    """set the transformation of an object"""
+
+    trans = transf['translation']
+    rot = transf['rotation']
+
+    obj.location = trans
+    # blender.point_at(cam, root_obj, 'TRACK_NEGATIVE_Z', 'UP_Y')
+
+    if isinstance(rot, dict):
+        point_at_obj = blender.get_obj_by_name(rot['point_at'])
+        blender.point_at(
+            obj,
+            point_at_obj,
+            blender.TRACK_AXIS[rot.get('track_axis', '-z')],
+            blender.UP_AXIS[rot.get('up_axis', 'y')])
+    else:
+        if len(rot) == 3:
+            # I think Panda3D's HPR is intrinsic
+            # If Blender is extrinsic, the order can just be reversed, LOL
+            # rot is in HPR form
+            # H -> Z
+            # P -> X
+            # R -> Y
+            obj.rotation_mode = 'ZXY'
+            obj.rotation_euler = (
+                math.radians(rot[1]),  # X == (ZXY)[1]
+                math.radians(rot[2]),  # Y == (ZXY)[2]
+                math.radians(rot[0]),  # Z == (ZXY)[0]
+            )
+        else:
+            obj.rotation_mode = 'QUATERNION'
+            obj.rotation_quaternion = rot
+
+
+def import_obj(filename) -> bpy.types.Object:
+    """import an obj file"""
+    bpy.ops.import_scene.obj(
+        filepath=filename,
+        use_smooth_groups=False,  # for now
+        split_mode='OFF',
+        axis_forward='Y',
+        axis_up='Z')
+    obj = bpy.context.selected_objects[0]
+    return obj
