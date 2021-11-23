2fc3905268aa6533d25dd2c0f854978fea5143db
Ben Zimmer <ben.d.zimmer@gmail.com>
2021-11-23 00:41:05 -0600

    test for util

---
+++ modeling/test_util.py
@@ -0,0 +1,32 @@
+"""
+
+Unit tests for modeling utilities.
+
+"""
+
+# Copyright (c) 2021 Ben Zimmer. All rights reserved.
+
+from modeling import util, components
+from modeling.types import vec3
+
+
+def test_union():
+    """unit test for wrapper around trimesh union"""
+
+    # all of this works with trimesh 3.2.2
+
+    # no redundant points
+    mesh = components.cylinder(4.0, 1.0, 8)
+    mesh_2 = util.translate_mesh(mesh, vec3(0.5, 0, 0))
+
+    # concat / union meshes together
+    mesh_c = util.concat([mesh, mesh_2])
+    mesh_u = util.union([mesh, mesh_2])
+
+    # union the mesh with just itself
+    mesh_u_self = util.union([mesh])
+
+    # util.view_mesh(mesh)
+    # util.view_mesh(mesh_c)
+    util.view_mesh(mesh_u)
+    util.view_mesh(mesh_u_self)
